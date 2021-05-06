8bb2be82925676b9d61a1759abfa6a82cb6e2fa5
Kishan Oza <ikishan.oza@gmail.com>
2021-05-06 12:05:16 +0530

    added number of days to check env and vaccine list env

--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -9,12 +9,13 @@ const BASE_URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/f
 const PINCODE = process.env.PINCODE
 const EMAIL = process.env.EMAIL
 const AGE = process.env.AGE
-
+const NO_OF_DAYS_TO_CHECK = Number(process.env.NO_OF_DAYS_TO_CHECK) || 10
+const PREFERRED_VACCINE_LIST = process.env.PREFERRED_VACCINE.split(',')
 async function mailFunction(){
     try {
-        cron.schedule('* * * * *', async () => {
-             await checkVaccineAvailability();
-        });
+        cron.schedule('*/5 * * * *', async () => {
+            await checkVaccineAvailability();
+       });
     } catch (e) {
         console.log('Error Occured: ' + JSON.stringify(e, null, 2));
         throw e;
@@ -23,7 +24,7 @@ async function mailFunction(){
 
 async function checkVaccineAvailability() {
 
-    let datesArray = await fetchNext10Days();
+    let datesArray = await fetchNextDays();
     datesArray.forEach(date => {
         getAvailableSlotsForDay(date);
     })
@@ -42,7 +43,7 @@ function getAvailableSlotsForDay(DATE) {
     axios(config)
         .then(function (slots) {
             let activeSession = slots.data.sessions;
-            let validSlots = activeSession.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0)
+            let validSlots = activeSession.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0 && (PREFERRED_VACCINE_LIST.length >0 || PREFERRED_VACCINE_LIST.indexOf(slot.vaccine)))
             if(validSlots.length > 0) {
                 notifyByEmail(validSlots);
             }
@@ -63,10 +64,10 @@ notifyByEmail(validSlots){
     })
 };
 
-async function fetchNext10Days(){
+async function fetchNextDays(){
     let dates = [];
     let today = moment();
-    for(let i = 0 ; i < 10 ; i ++ ){
+    for(let i = 0 ; i < NO_OF_DAYS_TO_CHECK ; i ++ ){
         let dateString = today.format('DD-MM-YYYY')
         dates.push(dateString);
         today.add(1, 'day');
